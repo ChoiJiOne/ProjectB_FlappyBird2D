167f99323a6a0b1237e61bfa7a7266a50c265f48
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-03-24 13:45:43 +0900

    [SETTING] 배치 스크립트에서 복사하는 기능을 파이썬 스크립트로 이동 및 자동으로 Visual Studio를 켜지는 기능 추가

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,29 +1,15 @@
 @echo off
 
 SET visualstudio=%1
-SET debugpath=FlappyBird2D\\bin\\Win64\\Debug
-SET releasepath=FlappyBird2D\\bin\\Win64\\Release
-SET shippingpath=FlappyBird2D\\bin\\Win64\\Shipping
-SET dllpath=ThirdParty\\Lib
+SET premake5=Bin\\premake5.exe
+SET project=FlappyBird2D
 
 if "%visualstudio%" == "" (
     echo make a choice visual studio version...
     GOTO:EOF
 )
 
-Bin\\premake5.exe %visualstudio%
-
-if not exist debugpath (
-    mkdir %debugpath%
-    xcopy /Y /S /Q %dllpath%\*.dll %debugpath%\
-)
-
-if not exist releasepath (
-    mkdir %releasepath%
-    xcopy /Y /S /Q %dllpath%\*.dll %releasepath%\
-)
-
-if not exist shippingpath (
-    mkdir %shippingpath%
-    xcopy /Y /S /Q %dllpath%\*.dll %shippingpath%\
-)
+%premake5% %visualstudio%
+python Script\\CopyDLL.py
+start %project%\\%project%.sln
+PAUSE
