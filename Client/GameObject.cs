6f745d0c3dd976bfb41d7c205de37eded66073ac
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-03-27 13:39:15 +0900

    [CLIENT] 게임 오브젝트 인터페이스 선언
    - 게임 내의 모든 오브젝트는 이 인터페이스를 상속받도록 구현 방향 설정

---
+++ Client/GameObject.cs
@@ -0,0 +1,35 @@
+using System;
+
+
+/**
+ * @brief 게임 내의 오브젝트를 나타내는 인터페이스입니다.
+ */
+public interface IGameObject
+{
+    /**
+     * @brief 게임 오브젝트를 초기화합니다.
+     */
+    public void Setup();
+
+
+    /**
+     * @brief 게임 오브젝트를 업데이트합니다.
+     * 
+     * @param DeltaSeconds 초단위 델타 시간값입니다.
+     */
+    public void Update(float DeltaSeconds);
+
+
+    /**
+     * @brief 게임 오브젝트를 화면에 그립니다.
+     * 
+     * @param Renderer 오브젝트를 화면에 그리기 위한 렌더러입니다.
+     */
+    public void Render(IntPtr Renderer);
+
+
+    /**
+     * @brief 게임 오브젝트 내의 리소스를 정리합니다.
+     */
+    public void Cleanup();
+}
