51f5aeb476807fae4d9ca098340e7dcfa0691937
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-03-28 17:17:27 +0900

    [CLIENT] 바닥을 그리는 메서드를 왼쪽 영역과 오른쪽 영역을 그리는 메서드로 분리

--- Client/Floor.cs
+++ Client/Floor.cs
@@ -36,7 +36,7 @@ class Floor : IGameObject
 
 
     /**
-     * @brief 백그라운드 게임 오브젝트를 업데이트합니다.
+     * @brief 게임의 바닥 오브젝트를 업데이트합니다.
      * 
      * @param DeltaSeconds 초단위 델타 시간값입니다.
      */
@@ -55,59 +55,21 @@ class Floor : IGameObject
 
 
     /**
-     * @brief 백그라운드 게임 오브젝트를 화면에 그립니다.
+     * @brief 게임의 바닥 오브젝트를 화면에 그립니다.
      * 
-     * @param Renderer 오브젝트를 화면에 그리기 위한 렌더러입니다.
+     * @param renderer 오브젝트를 화면에 그리기 위한 렌더러입니다.
      */
-    public void Render(IntPtr Renderer)
+    public void Render(IntPtr renderer)
     {
-        float lerp = accumulateTime_ / speed_;
+        float factor = accumulateTime_ / speed_;
 
-        float width = texture_.Width;
-        float height = texture_.Height;
-
-        SDL.SDL_Rect leftSrcRect;
-        leftSrcRect.x = (int)(width * lerp);
-        leftSrcRect.y = 0;
-        leftSrcRect.w = (int)(width * (1.0f - lerp));
-        leftSrcRect.h = (int)height;
-
-        SDL.SDL_Rect leftDstRect;
-        leftDstRect.x = (int)(rigidBody_.Center.x - rigidBody_.Width / 2.0f);
-        leftDstRect.y = (int)(rigidBody_.Center.y - rigidBody_.Height / 2.0f);
-        leftDstRect.w = (int)(rigidBody_.Width * (1.0f - lerp));
-        leftDstRect.h = (int)(rigidBody_.Height);
-
-        SDL.SDL_RenderCopy(
-            Renderer,
-            texture_.Resource,
-            ref leftSrcRect,
-            ref leftDstRect
-        );
-
-        SDL.SDL_Rect rightSrcRect;
-        rightSrcRect.x = 0;
-        rightSrcRect.y = 0;
-        rightSrcRect.w = (int)(width * lerp);
-        rightSrcRect.h = (int)height;
-
-        SDL.SDL_Rect rightDstRect;
-        rightDstRect.x = (int)(rigidBody_.Center.x - rigidBody_.Width / 2.0f + rigidBody_.Width * (1.0f - lerp));
-        rightDstRect.y = (int)(rigidBody_.Center.y - rigidBody_.Height / 2.0f);
-        rightDstRect.w = (int)(rigidBody_.Width * lerp);
-        rightDstRect.h = (int)(rigidBody_.Height);
-        
-        SDL.SDL_RenderCopy(
-             Renderer,
-             texture_.Resource,
-             ref rightSrcRect,
-             ref rightDstRect
-         );
+        RenderLeftSide(renderer, factor);
+        RenderRightSide(renderer, factor);
     }
 
 
     /**
-     * @brief 백그라운드 게임 오브젝트 내의 리소스를 정리합니다.
+     * @brief 게임의 바닥 오브젝트 내의 리소스를 정리합니다.
      */
     public void Cleanup()
     {
@@ -116,9 +78,75 @@ class Floor : IGameObject
 
 
     /**
+     * @brief 바닥의 왼쪽 영역을 그립니다.
+     * 
+     * @param renderer 바닥의 왼쪽 영역을 그릴 때 사용할 렌더러입니다.
+     * @param factor 왼쪽 영역의 비율입니다.
+     */
+    private void RenderLeftSide(IntPtr renderer, float factor)
+    {
+        float width = texture_.Width;
+        float height = texture_.Height;
+
+        SDL.SDL_Rect leftSrcRect;
+        leftSrcRect.x = (int)(width * factor);
+        leftSrcRect.y = 0;
+        leftSrcRect.w = (int)(width * (1.0f - factor));
+        leftSrcRect.h = (int)height;
+
+        SDL.SDL_Rect leftDstRect;
+        leftDstRect.x = (int)(rigidBody_.Center.x - rigidBody_.Width / 2.0f);
+        leftDstRect.y = (int)(rigidBody_.Center.y - rigidBody_.Height / 2.0f);
+        leftDstRect.w = (int)(rigidBody_.Width * (1.0f - factor));
+        leftDstRect.h = (int)(rigidBody_.Height);
+
+        SDL.SDL_RenderCopy(
+            renderer,
+            texture_.Resource,
+            ref leftSrcRect,
+            ref leftDstRect
+        );
+    }
+
+
+    /**
+     * @brief 바닥의 오른쪽 영역을 그립니다.
+     * 
+     * @note 오른쪽 영역을 그릴 때, 왼쪽 영역을 기준으로 그립니다.
+     * 
+     * @param renderer 바닥의 오른쪽 영역을 그릴 때 사용할 렌더러입니다.
+     * @param factor 왼쪽 영역의 비율입니다.
+     */
+    private void RenderRightSide(IntPtr renderer, float factor)
+    {
+        float width = texture_.Width;
+        float height = texture_.Height;
+
+        SDL.SDL_Rect rightSrcRect;
+        rightSrcRect.x = 0;
+        rightSrcRect.y = 0;
+        rightSrcRect.w = (int)(width * factor);
+        rightSrcRect.h = (int)height;
+
+        SDL.SDL_Rect rightDstRect;
+        rightDstRect.x = (int)(rigidBody_.Center.x - rigidBody_.Width / 2.0f + rigidBody_.Width * (1.0f - factor));
+        rightDstRect.y = (int)(rigidBody_.Center.y - rigidBody_.Height / 2.0f);
+        rightDstRect.w = (int)(rigidBody_.Width * factor);
+        rightDstRect.h = (int)(rigidBody_.Height);
+
+        SDL.SDL_RenderCopy(
+             renderer,
+             texture_.Resource,
+             ref rightSrcRect,
+             ref rightDstRect
+        );
+    }
+
+
+    /**
      * @brief 게임의 바닥이 현재 이동 중인지 확인합니다.
      */
-    bool bIsMove_ = false;
+    private bool bIsMove_ = false;
 
 
     /**
