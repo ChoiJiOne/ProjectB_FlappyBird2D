44789f530f5bafb9d872bb3a061e1f726fadeafa
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-01 17:01:36 +0900

    [SCRIPT] 빌드 전에 Visual Studio 솔루션을 업데이트하는 기능 추가

--- Build.bat
+++ Build.bat
@@ -6,11 +6,13 @@ SET build=msbuild.exe
 SET project=FlappyBird2D
 SET visualstudio=%1
 SET option=%2
+SET premake5=Bin\\premake5.exe
 
 if not exist %project% (
     echo can't find %project% visual studio solution
-    echo generate visual studio solution...
-    Bin\\premake5.exe %visualstudio%
 )
+
+echo run premake5.exe...
+%premake5% %visualstudio%
 
 %build% %project%\\%project%.sln -property:Configuration=%option% -target:Rebuild
