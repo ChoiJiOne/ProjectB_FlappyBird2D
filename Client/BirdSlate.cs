4c9bfb0f1fc56f328162a03ecda447a9e1fa0b98
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-05 09:56:41 +0900

    [CLIENT] BirdSlate가 SlideSlate를 상속받도록 구조 변경

--- Client/BirdSlate.cs
+++ Client/BirdSlate.cs
@@ -1,27 +1,11 @@
 /**
  * @brief 게임 내의 새 슬레이트 UI 오브젝트입니다.
  */
-class BirdSlate : Slate
+class BirdSlate : SlideSlate
 {
     /**
      * @brief 게임 내의 새 슬레이트 UI 오브젝트 속성에 대한 Getter/Setter입니다.
      */
-    public bool Movable
-    {
-        get => bCanMove_;
-        set => bCanMove_ = value;
-    }
-
-    public float MaxWaitTimeForMove
-    {
-        set => maxWaitTimeForMove_ = value;
-    }
-
-    public float MoveLength
-    {
-        set => moveLength_ = value;
-    }
-
     public float ChangeWingStateTime
     {
         set => changeWingStateTime_ = value;
@@ -37,33 +21,12 @@ class BirdSlate : Slate
     {
         if (bCanMove_)
         {
-            UpdateSlatePosition(deltaSeconds);
             UpdateWingState(deltaSeconds);
         }
 
         UpdateUITexture();
 
         base.Tick(deltaSeconds);
-    }
-
-
-    /**
-     * @brief 위치를 업데이트합니다.
-     * 
-     * @param deltaSeconds 초단위 델타 시간값입니다.
-     */
-    private void UpdateSlatePosition(float deltaSeconds)
-    {
-        waitTimeForMove_ += deltaSeconds;
-        if (waitTimeForMove_ > maxWaitTimeForMove_)
-        {
-            waitTimeForMove_ = 0.0f;
-            moveDirection_ *= -1.0f;
-        }
-
-        Vector2<float> center = UIBody.Center;
-        center.y += (moveDirection_ * deltaSeconds * moveLength_);
-        UIBody.Center = center;
     }
 
 
@@ -114,32 +77,6 @@ class BirdSlate : Slate
      * @brief 새 슬레이트 오브젝트가 움직일 수 있는지 확인합니다.
      */
     private bool bCanMove_ = true;
-
-
-    /**
-     * @brief 새 슬레이트 오브젝트가 움직이는 방향입니다.
-     * 
-     * @note +는 아래 방향, -는 위 방향입니다.
-     */
-    private float moveDirection_ = 1.0f;
-
-
-    /**
-     * @brief 새 슬레이트 오브젝트가 움직이기 위해 누적된 시간입니다.
-     */
-    private float waitTimeForMove_ = 0.0f;
-
-
-    /**
-     * @brief 새 슬레이트 오브젝트가 움직이기 위해 기다릴 수 있는 최대 시간입니다.
-     */
-    private float maxWaitTimeForMove_ = 0.0f;
-
-
-    /**
-     * @brief 새 슬레이트 오브젝트가 움직이는 거리입니다.
-     */
-    private float moveLength_ = 0.0f;
 
 
     /**
