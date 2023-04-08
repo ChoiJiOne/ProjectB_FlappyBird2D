14f6075db4c6becec7edf3f574e80f39e50252da
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-04-08 09:53:50 +0900

    [SETTING] Visual Studio 솔루션에 AudioModule 프로젝트 추가

--- premake5.lua
+++ premake5.lua
@@ -19,8 +19,55 @@ workspace "FlappyBird2D"
     -- 서버 소스 경로를 설정합니다.
     server="%{wks.location}/../Server"
 
+    -- 오디오 모듈 경로를 설정합니다.
+    audiomodule="%{wks.location}/../AudioModule"
+
     -- 서드파티 라이브러리 경로를 설정합니다.
     thirdparty="%{wks.location}/../ThirdParty"
+
+
+    -- 오디오 모듈 프로젝트입니다.
+    project "AudioModule"
+        -- 오디오 모듈 프로젝트의 종류를 설정합니다.
+        kind "SharedLib"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C++"
+
+        -- C++의 표준을 설정합니다.
+        cppdialect "C++17"
+
+        -- 오디오 모듈 소스 코드의 include 경로를 추가합니다.
+        includedirs { 
+            "%{audiomodule}/Source", 
+            "%{audiomodule}/ThirdParty",
+        }
+
+        -- 오디오 모듈 소스 코드의 file을 추가합니다.
+        files { 
+            "%{audiomodule}/Source/*", 
+            "%{audiomodule}/ThirdParty/*",
+        }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG", "RELEASE" }
+            runtime "Release"
+            optimize "On"
+            symbols "On"
+
+        filter "configurations:Shipping"
+            defines { "NDEBUG", "SHIPPING" }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
+
 
     -- 클라이언트 프로젝트입니다.
     project "Client"
@@ -58,13 +105,12 @@ workspace "FlappyBird2D"
         libdirs { "%{thirdparty}/Lib", }
 
         -- 외부 라이브러리를 추가합니다.
-        links { 
+        links {
             "System",
+            "AudioModule",
             "SDL2.lib", 
             "SDL2main.lib",
-            "SDL2_image.lib", 
-            "SDL2_mixer.lib", 
-            "SDL2_ttf.lib", 
+            "SDL2_image.lib",
         }
 
         -- 명령행 인수를 설정합니다.
