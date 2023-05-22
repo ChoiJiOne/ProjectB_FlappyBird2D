0a417d7206265d9e64ea4fb77ed8e8d0127b7e09
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-05-22 10:27:01 +0900

    [GAME] 오디오 모듈 인터페이스의 코딩 컨벤션 수정

--- Game/AudioModule.cs
+++ Game/AudioModule.cs
@@ -26,54 +26,54 @@ class AudioModule
     /**
      * @brief 사운드 리소스를 생성합니다.
      *
-     * @param Path 사운드 리소스의 경로입니다.
+     * @param path 사운드 리소스의 경로입니다.
      *
      * @return 생성된 사운드 리소스의 아이디를 반환합니다. 생성에 실패하면 -1을 반환합니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "CreateSound", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
-    public static extern int CreateSound([MarshalAs(UnmanagedType.LPStr)] string Path);
+    public static extern int CreateSound([MarshalAs(UnmanagedType.LPStr)] string path);
 
 
     /**
      * @brief 사운드의 크기를 설정합니다.
      *
-     * @param SoundID 크기를 설정할 사운드의 아이디입니다.
-     * @param Volume 사운드의 크기입니다. 범위는 0.0 ~ 1.0 입니다.
+     * @param soundID 크기를 설정할 사운드의 아이디입니다.
+     * @param volume 사운드의 크기입니다. 범위는 0.0 ~ 1.0 입니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "SetSoundVolume", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void SetSoundVolume(int SoundID, float Volume);
+    public static extern void SetSoundVolume(int soundID, float volume);
 
 
     /**
      * @brief 현재 사운드의 크기를 얻습니다.
      *
-     * @param SoundID 소리 크기를 얻을 사운드의 아이디입니다.
+     * @param soundID 소리 크기를 얻을 사운드의 아이디입니다.
      *
      * @return 현재 사운드의 크기를 얻습니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "GetSoundVolume", CallingConvention = CallingConvention.Cdecl)]
-    public static extern float GetSoundVolume(int SoundID);
+    public static extern float GetSoundVolume(int soundID);
 
 
     /**
      * @brief 사운드의 반복 여부를 설정합니다.
      *
-     * @param SoundID 반복 여부를 설정할 사운드의 아이디입니다.
+     * @param soundID 반복 여부를 설정할 사운드의 아이디입니다.
      * @param bIsLoop 사운드 반복 여부입니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "SetSoundLooping", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void SetSoundLooping(int SoundID, bool bIsLoop);
+    public static extern void SetSoundLooping(int soundID, bool bIsLoop);
 
 
     /**
      * @brief 사운드의 반복 여부를 얻습니다.
      *
-     * @param SoundID 반복 여부를 확인할 사운드의 아이디입니다.
+     * @param soundID 반복 여부를 확인할 사운드의 아이디입니다.
      *
      * @return 사운드가 반복한다면 true, 그렇지 않다면 false를 반환합니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "GetSoundLooping", CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool GetSoundLooping(int SoundID);
+    public static extern bool GetSoundLooping(int soundID);
 
 
     /**
@@ -81,21 +81,21 @@ class AudioModule
      *
      * @note 이전에 중지한 적이 있다면 중지한 시점부터 플레이됩니다.
      *
-     * @param SoundID 플레이할 사운드의 아이디입니다.
+     * @param soundID 플레이할 사운드의 아이디입니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "PlaySound", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void PlaySound(int SoundID);
+    public static extern void PlaySound(int soundID);
 
 
     /**
      * @brief 사운드가 플레이중인지 확인합니다.
      *
-     * @param SoundID 플레이 중인지 확인할 사운드 아이디입니다.
+     * @param soundID 플레이 중인지 확인할 사운드 아이디입니다.
      *
      * @return 사운드가 플레이 중이라면 true, 그렇지 않다면 false를 반환합니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "IsPlayingSound", CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool IsPlayingSound(int SoundID);
+    public static extern bool IsPlayingSound(int soundID);
 
 
     /**
@@ -106,27 +106,27 @@ class AudioModule
      * @return 사운드 플레이가 끝났다면 true, 그렇지 않다면 false를 반환합니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "IsDoneSound", CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool IsDoneSound(int SoundID);
+    public static extern bool IsDoneSound(int soundID);
 
 
     /**
      * @brief 사운드 플레이를 중지합니다.
      *
-     * @param SoundID 플레이를 중지할 사운드 아이디입니다.
+     * @param soundID 플레이를 중지할 사운드 아이디입니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "StopSound", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void StopSound(int SoundID);
+    public static extern void StopSound(int soundID);
 
 
     /**
      * @brief 사운드 플레이가 중지 되었는지 확인합니다.
      *
-     * @param SoundID 플레이가 중지 되었는지 확인할 사운드 아이디입니다.
+     * @param soundID 플레이가 중지 되었는지 확인할 사운드 아이디입니다.
      *
      * @return 사운드 플레이가 중지 되었다면 true, 그렇지 않으면 false를 반환합니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "IsStoppingSound", CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool IsStoppingSound(int SoundID);
+    public static extern bool IsStoppingSound(int soundID);
 
 
     /**
@@ -135,5 +135,5 @@ class AudioModule
      * @param 초기화할 사운드의 아이디입니다.
      */
     [DllImport("AudioModule.dll", EntryPoint = "ResetSound", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void ResetSound(int SoundID);
+    public static extern void ResetSound(int soundID);
 }
