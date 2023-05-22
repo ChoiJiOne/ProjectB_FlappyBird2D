91901e694d6fecc033e1e0e5176260f8bb3f8493
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-05-22 11:18:51 +0900

    [TOOL] 평가식이 거짓으로 평가되면 예외를 던지는 매크로 추가
    - 강제로 예외를 던지는 매크로 추가

--- CommonTool/Public/Macro.h
+++ CommonTool/Public/Macro.h
@@ -15,3 +15,37 @@ CLASS(const CLASS&) = delete;\
 CLASS& operator=(CLASS&&) = delete;\
 CLASS& operator=(const CLASS&) = delete;
 #endif
+
+
+/**
+ * @brief 평가식을 검사하고 거짓으로 평가되면 C++ 표준 예외를 던집니다.
+ *
+ * @param EXPRESSION 검사할 평가식입니다.
+ * @param MESSAGE 평가식이 거짓으로 평가될 경우의 메시지입니다.
+ *
+ * @throws 평가식이 거짓으로 평가될 경우, C++ 표준 예외를 던집니다.
+ */
+#ifndef CHECK
+#define CHECK(EXPRESSION, MESSAGE)\
+{\
+	if(!EXPRESSION)\
+	{\
+		throw std::exception();\
+	}\
+}
+#endif
+
+
+/**
+ * @brief 강제로 C++ 표준 예외를 던집니다.
+ *
+ * @param MESSAGE 예외가 발생했을 때의 메시지입니다.
+ *
+ * @throws C++ 표준 예외를 던집니다.
+ */
+#ifndef ENFORCE_THROW_EXCEPTION
+#define ENFORCE_THROW_EXCEPTION(MESSAGE)\
+{\
+	throw std::exception();\
+}
+#endif
