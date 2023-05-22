0b26d787484f6ccb4296be9c00a16a4409f45bfb
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-05-22 15:35:14 +0900

    [TOOL/TEST] 섹션 내 데이터를 삭제하는 테스트 코드 추가

--- FontAltasTool/Application.cpp
+++ FontAltasTool/Application.cpp
@@ -27,8 +27,21 @@ int32_t main(int32_t argc, char** argv)
 	section.AddData("C", "3");
 	section.AddData("D", "4");
 
-	auto sectionData = section.GetSectionData();
-	for (auto data : sectionData)
+	auto sectionData0 = section.GetSectionData();
+	for (auto data : sectionData0)
+	{
+		std::cout << data.first << ", " << data.second << std::endl;
+	}
+
+	auto data1 = section.GetData("A");
+	std::cout << data1.first << ", " << data1.second << std::endl;
+
+	std::string data2 = section.GetValue("B");
+	std::cout << data2 << std::endl;
+
+	section.RemoveData("C");
+	auto sectionData1 = section.GetSectionData();
+	for (auto data : sectionData1)
 	{
 		std::cout << data.first << ", " << data.second << std::endl;
 	}
