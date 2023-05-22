b50ea947cda6403aa6ec3288e88b07fe0de704c5
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-05-22 10:23:18 +0900

    [MODULE] 오디오 모듈 내 소스의 코딩 컨벤션 올바르게 수정

--- AudioModule/AudioModule.cpp
+++ AudioModule/AudioModule.cpp
@@ -16,30 +16,30 @@ static bool bIsSetup = false;
 /**
  * @brief AudioManager에서 생성한 사운드 리소스 수입니다.
  */
-static int32_t CountSoundResource = 0;
+static int32_t countSoundResource = 0;
 
 /**
  * @brief miniaudio 엔진입니다.
  */
-static std::unique_ptr<ma_engine> MiniAudioEngine = nullptr;
+static std::unique_ptr<ma_engine> miniAudioEngine = nullptr;
 
 /**
  * @brief 사운드 리소스 오브젝트입니다.
  */
-static std::vector<std::unique_ptr<ma_sound>> Sounds;
+static std::vector<std::unique_ptr<ma_sound>> sounds;
 
 
 bool Setup()
 {
 	if (bIsSetup) return false;
 
-	MiniAudioEngine = std::make_unique<ma_engine>();
-	if (ma_engine_init(nullptr, MiniAudioEngine.get()) != MA_SUCCESS)
+	miniAudioEngine = std::make_unique<ma_engine>();
+	if (ma_engine_init(nullptr, miniAudioEngine.get()) != MA_SUCCESS)
 	{
 		return false;
 	}
 
-	Sounds.resize(0);
+	sounds.resize(0);
 	bIsSetup = true;
 
 	return true;
@@ -49,98 +49,98 @@ void Cleanup()
 {
 	if (!bIsSetup) return;
 
-	for (auto& Sound : Sounds)
+	for (auto& sound : sounds)
 	{
-		ma_sound_uninit(Sound.get());
-		Sound.reset();
+		ma_sound_uninit(sound.get());
+		sound.reset();
 	}
 
-	ma_engine_uninit(MiniAudioEngine.get());
-	MiniAudioEngine.reset();
+	ma_engine_uninit(miniAudioEngine.get());
+	miniAudioEngine.reset();
 
 	bIsSetup = false;
 }
 
-int32_t CreateSound(const char* Path)
+int32_t CreateSound(const char* path)
 {
-	std::unique_ptr<ma_sound> Sound = std::make_unique<ma_sound>();
+	std::unique_ptr<ma_sound> sound = std::make_unique<ma_sound>();
 
-	if (ma_sound_init_from_file(MiniAudioEngine.get(), Path, 0, nullptr, nullptr, Sound.get()) != MA_SUCCESS)
+	if (ma_sound_init_from_file(miniAudioEngine.get(), path, 0, nullptr, nullptr, sound.get()) != MA_SUCCESS)
 	{
 		return -1;
 	}
 
-	Sounds.push_back(std::move(Sound));
-	return CountSoundResource++;
+	sounds.push_back(std::move(sound));
+	return countSoundResource++;
 }
 
-void SetSoundVolume(int32_t SoundID, float Volume)
+void SetSoundVolume(int32_t soundID, float volume)
 {
-	if (SoundID == -1) return;
+	if (soundID == -1) return;
 
-	Volume = std::clamp<float>(Volume, 0.0f, 1.0f);
-	ma_sound_set_volume(Sounds[SoundID].get(), Volume);
+	volume = std::clamp<float>(volume, 0.0f, 1.0f);
+	ma_sound_set_volume(sounds[soundID].get(), volume);
 }
 
-float GetSoundVolume(int32_t SoundID)
+float GetSoundVolume(int32_t soundID)
 {
-	if (SoundID == -1) return 0.0f;
+	if (soundID == -1) return 0.0f;
 
-	return ma_sound_get_volume(Sounds[SoundID].get());
+	return ma_sound_get_volume(sounds[soundID].get());
 }
 
-void SetSoundLooping(int32_t SoundID, bool bIsLoop)
+void SetSoundLooping(int32_t soundID, bool bIsLoop)
 {
-	if (SoundID == -1) return;
+	if (soundID == -1) return;
 
-	ma_sound_set_looping(Sounds[SoundID].get(), static_cast<ma_bool32>(bIsLoop));
+	ma_sound_set_looping(sounds[soundID].get(), static_cast<ma_bool32>(bIsLoop));
 }
 
-bool GetSoundLooping(int32_t SoundID)
+bool GetSoundLooping(int32_t soundID)
 {
-	if (SoundID == -1) return false;
+	if (soundID == -1) return false;
 
-	return ma_sound_is_looping(Sounds[SoundID].get()) == MA_TRUE;
+	return ma_sound_is_looping(sounds[soundID].get()) == MA_TRUE;
 }
 
-void PlaySound(int32_t SoundID)
+void PlaySound(int32_t soundID)
 {
-	if (SoundID == -1) return;
+	if (soundID == -1) return;
 
-	ma_sound_start(Sounds[SoundID].get());
+	ma_sound_start(sounds[soundID].get());
 }
 
-bool IsPlayingSound(int32_t SoundID)
+bool IsPlayingSound(int32_t soundID)
 {
-	if (SoundID == -1) return false;
+	if (soundID == -1) return false;
 
-	return ma_sound_is_playing(Sounds[SoundID].get()) == MA_TRUE;
+	return ma_sound_is_playing(sounds[soundID].get()) == MA_TRUE;
 }
 
-bool IsDoneSound(int32_t SoundID)
+bool IsDoneSound(int32_t soundID)
 {
-	if (SoundID == -1) return false;
+	if (soundID == -1) return false;
 
-	return ma_sound_at_end(Sounds[SoundID].get()) == MA_TRUE;
+	return ma_sound_at_end(sounds[soundID].get()) == MA_TRUE;
 }
 
-void StopSound(int32_t SoundID)
+void StopSound(int32_t soundID)
 {
-	if (SoundID == -1) return;
+	if (soundID == -1) return;
 
-	ma_sound_stop(Sounds[SoundID].get());
+	ma_sound_stop(sounds[soundID].get());
 }
 
-bool IsStoppingSound(int32_t SoundID)
+bool IsStoppingSound(int32_t soundID)
 {
-	if (SoundID == -1) return false;
+	if (soundID == -1) return false;
 
-	return !IsDoneSound(SoundID) && !IsPlayingSound(SoundID);
+	return !IsDoneSound(soundID) && !IsPlayingSound(soundID);
 }
 
-void ResetSound(int32_t SoundID)
+void ResetSound(int32_t soundID)
 {
-	if (SoundID == -1) return;
+	if (soundID == -1) return;
 
-	ma_sound_seek_to_pcm_frame(Sounds[SoundID].get(), 0);
+	ma_sound_seek_to_pcm_frame(sounds[soundID].get(), 0);
 }
