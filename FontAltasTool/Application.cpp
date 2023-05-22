017fd89abb40427837087afde4c4d597f6e117d8
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-05-22 20:44:56 +0900

    [TOOL/TEST] 기존의 테스트 코드 전체 삭제

--- FontAltasTool/Application.cpp
+++ FontAltasTool/Application.cpp
@@ -22,22 +22,5 @@
  */
 int32_t main(int32_t argc, char** argv)
 {
-	INIFormat iniFormat;
-
-	INISection section0;
-	section0.AddData("A", "1");
-	section0.AddData("B", "2");
-	section0.AddData("C", "3");
-	section0.AddData("D", "4");
-	iniFormat.AddSection("x", section0);
-	
-	INISection section1;
-	section1.AddData("E", "1");
-	section1.AddData("F", "2");
-	section1.AddData("G", "3");
-	section1.AddData("H", "4");
-	iniFormat.AddSection("y", section1);
-	
-	INIFormat::ExportINIFile("D:\\Work\\FlappyBird2D\\Content\\test.ini", iniFormat);
 	return 0;
 }
