559c09670c25ce7abb0eb3d6cef9564e979f295a
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-05-23 17:11:02 +0900

    [TOOL] 명령행 인자를 파싱하고 인자를 검사하는 기능 추가

--- FontAltasTool/Application.cpp
+++ FontAltasTool/Application.cpp
@@ -4,9 +4,11 @@
 #include <stb_image_write.h>
 // @third party code - END
 
+#include <array>
 #include <iostream>
 #include <vector>
 
+#include "CommandLine.h"
 #include "FileHelper.hpp"
 #include "Glyph.h"
 #include "INIFormat.h"
@@ -23,5 +25,25 @@
  */
 int32_t main(int32_t argc, char** argv)
 {
+	CommandLine::Parse(argc, argv);
+
+	std::array<std::string, 6> arguments = {
+		"Crash",
+		"FontPath",
+		"BeginCodePoint",
+		"EndCodePoint",
+		"FontSize",
+		"OutputPath",
+	};
+
+	for (const auto& argument : arguments)
+	{
+		if (!CommandLine::IsValid(argument))
+		{
+			std::cout << "invaild command line argument" << std::endl;
+			return -1;
+		}
+	}
+	
 	return 0;
 }
