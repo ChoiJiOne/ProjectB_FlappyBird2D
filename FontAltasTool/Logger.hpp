d3d10bde19a5e5a8f17f3af99dea3a49bfddab73
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-05-27 17:51:50 +0900

    [TOOL] 콘솔 텍스트의 색상을 설정하는 기능 추가

--- FontAltasTool/Logger.hpp
+++ FontAltasTool/Logger.hpp
@@ -3,6 +3,8 @@
 #include <cstdint>
 #include <windows.h>
 
+#include "Macro.h"
+
 
 /**
  * @brief 로그 처리에 대한 헬퍼 기능을 수행합니다.
@@ -34,4 +36,18 @@ private:
 		LIGHTYELLOW = 14,
 		BRIGHTWHITE = 15
 	};
+
+
+private:
+	/**
+	 * @brief 콘솔 창의 텍스트 색상을 설정합니다.
+	 *
+	 * @param consoleColor 설정할 색상입니다.
+	 *
+	 * @throws 설정에 실패하면 C++ 표준 예외를 던집니다.
+	 */
+	static inline void SetConsoleColor(const EConsoleColor& consoleColor)
+	{
+		CHECK(SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), static_cast<WORD>(consoleColor)), "failed to set text color...");
+	}
 };
