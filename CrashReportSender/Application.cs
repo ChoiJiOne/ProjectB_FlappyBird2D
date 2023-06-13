e44589a64c375f2110b8c69ebfe6f1c9e589a0d4
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-06-13 15:51:14 +0900

    [TOOL] 크래시 덤프와 로그 파일을 전송하기 위한 빈 소스 파일 추가

---
+++ CrashReportSender/Application.cs
@@ -0,0 +1,15 @@
+/**
+ * @brief 크래시 덤프 파일과 로그 파일을 전송하는 애플리케이션을 실행합니다.
+ */
+class CrashReportSender
+{
+    /**
+     * @brief 크래시 덤프 파일과 로그 파일을 전송하는 애플리케이션을 실행합니다.
+     * 
+     * @param args 명령행 인수입니다.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+}
