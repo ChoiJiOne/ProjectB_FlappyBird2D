d758b79272afa9141984ddf3a2f80ea932ca3387
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-06-13 16:04:57 +0900

    [TOOL] TODO : 패킷의 헤더, 바디 선언 및 구현

--- CrashPacket/CrashPacket.cs
+++ CrashPacket/CrashPacket.cs
@@ -1,6 +1,19 @@
-using System;
-
-class PacketHeader : ISerializable
+/**
+ * @brief 직렬화된 패킷의 헤더입니다.
+ */
+class PacketHeader : 
 {
 
 }
+
+
+
+/**
+ * @brief 직렬화된 패킷의 바디입니다.
+ */
+
+
+
+/**
+ * @brief 직렬화된 패킷입니다.
+ */
