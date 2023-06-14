0b95cb5d49675dae61785bd1a993054804e35607
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-06-14 10:46:22 +0900

    [TOOL] 파일 전송 요청 부분과 파일 전송 부분을 메서드로 분리

--- CrashReportSender/Application.cs
+++ CrashReportSender/Application.cs
@@ -22,80 +22,9 @@ class CrashReportSender
         client_.Connect(serverAddress_);
         networkStream_ = client_.GetStream();
 
-        { // 파일 전송 요청
-            CrashPacket requestPacket = new CrashPacket();
-            requestPacket.Body = new CrashPacketRequestBody()
-            {
-                FileSize = new FileInfo(sendFilePath_).Length,
-                FileName = System.Text.Encoding.Default.GetBytes(sendFilePath_)
-            };
-            requestPacket.Header = new CrashPacketHeader()
-            {
-                PacketID = crashPacketID_++,
-                PacketType = (uint)(CrashPacket.EType.REQ_FILE_SEND),
-                BodySize = (uint)(requestPacket.Body.GetByteBufferSize()),
-                Fragmented = (byte)(CrashPacket.EFragment.NO),
-                LastPacket = (byte)(CrashPacket.ELast.YES),
-                Seq = 0
-            };
-            CrashPacket.Send(networkStream_, requestPacket);
+        RequestSendFileToServer();
+        SendFileToServer();
 
-            CrashPacket responePacket = CrashPacket.Receive(networkStream_);
-            CrashPacketHeader packetHeader = (CrashPacketHeader)(responePacket.Header);
-
-            if(packetHeader.PacketType != (uint)(CrashPacket.EType.REP_FILE_SEND))
-            {
-                Console.WriteLine("invalid response from crash collector...");
-                networkStream_.Close();
-                client_.Close();
-                return;
-            }
-        }
-        
-        { // 파일 전송
-            Stream fileStream = new FileStream(sendFilePath_, FileMode.Open);
-            byte[] readbytes = new byte[FILE_CHUNK_SIZE];
-            long readValue = BitConverter.ToInt64(readbytes, 0);
-            int totalRead = 0;
-            ushort packetSeq = 0;
-            byte fragmented = (byte)((fileStream.Length < FILE_CHUNK_SIZE) ? CrashPacket.EFragment.NO : CrashPacket.EFragment.YES);
-
-            while (totalRead < fileStream.Length)
-            {
-                int read = fileStream.Read(readbytes, 0, FILE_CHUNK_SIZE);
-                totalRead += read;
-                CrashPacket filePacket = new CrashPacket();
-
-                byte[] sendBytes = new byte[read];
-                Array.Copy(readbytes, 0, sendBytes, 0, read);
-
-                filePacket.Body = new CrashPacketDataBody(sendBytes);
-                filePacket.Header = new CrashPacketHeader()
-                {
-                    PacketID = crashPacketID_,
-                    PacketType = (uint)(CrashPacket.EType.FILE_SEND_DATA),
-                    BodySize = (uint)(filePacket.Body.GetByteBufferSize()),
-                    Fragmented = fragmented,
-                    LastPacket = (byte)((totalRead < fileStream.Length) ? CrashPacket.ELast.NO : CrashPacket.ELast.YES),
-                    Seq = packetSeq++
-                };
-
-                CrashPacket.Send(networkStream_, filePacket);
-            }
-
-            CrashPacket resultPacket = CrashPacket.Receive(networkStream_);
-            CrashPacketResultBody resultPacketBody = (CrashPacketResultBody)(resultPacket.Body);
-
-            if(resultPacketBody.Result == (byte)(CrashPacket.ESuccess.SUCCESS))
-            {
-                Console.WriteLine("successed sending crash report file...");
-            }
-            else
-            {
-                Console.WriteLine("failed sending crash report file...");
-            }
-        }
-        
         networkStream_.Close();
         client_.Close();
     }
@@ -120,6 +49,89 @@ class CrashReportSender
         Console.WriteLine("IP = {0}", serverIP_);
         Console.WriteLine("PORT = {0}", serverPort_);
         Console.WriteLine("FILE = {0}", sendFilePath_);
+    }
+
+
+    /**
+     * @brief 파일 전송 요청을 서버로 보냅니다.
+     */
+    private static void RequestSendFileToServer()
+    {
+        CrashPacket requestPacket = new CrashPacket();
+        requestPacket.Body = new CrashPacketRequestBody()
+        {
+            FileSize = new FileInfo(sendFilePath_).Length,
+            FileName = System.Text.Encoding.Default.GetBytes(sendFilePath_)
+        };
+        requestPacket.Header = new CrashPacketHeader()
+        {
+            PacketID = crashPacketID_++,
+            PacketType = (uint)(CrashPacket.EType.REQ_FILE_SEND),
+            BodySize = (uint)(requestPacket.Body.GetByteBufferSize()),
+            Fragmented = (byte)(CrashPacket.EFragment.NO),
+            LastPacket = (byte)(CrashPacket.ELast.YES),
+            Seq = 0
+        };
+        CrashPacket.Send(networkStream_, requestPacket);
+
+        CrashPacket responePacket = CrashPacket.Receive(networkStream_);
+        CrashPacketHeader packetHeader = (CrashPacketHeader)(responePacket.Header);
+
+        if (packetHeader.PacketType != (uint)(CrashPacket.EType.REP_FILE_SEND))
+        {
+            Console.WriteLine("invalid response from crash collector...");
+            networkStream_.Close();
+            client_.Close();
+            return;
+        }
+    }
+
+
+    /**
+     * @brief 파일을 서버로 보냅니다.
+     */
+    private static void SendFileToServer()
+    {
+        Stream fileStream = new FileStream(sendFilePath_, FileMode.Open);
+        byte[] readbytes = new byte[FILE_CHUNK_SIZE];
+        int totalRead = 0;
+        ushort packetSeq = 0;
+        byte fragmented = (byte)((fileStream.Length < FILE_CHUNK_SIZE) ? CrashPacket.EFragment.NO : CrashPacket.EFragment.YES);
+
+        while (totalRead < fileStream.Length)
+        {
+            int read = fileStream.Read(readbytes, 0, FILE_CHUNK_SIZE);
+            totalRead += read;
+            CrashPacket filePacket = new CrashPacket();
+
+            byte[] sendBytes = new byte[read];
+            Array.Copy(readbytes, 0, sendBytes, 0, read);
+
+            filePacket.Body = new CrashPacketDataBody(sendBytes);
+            filePacket.Header = new CrashPacketHeader()
+            {
+                PacketID = crashPacketID_,
+                PacketType = (uint)(CrashPacket.EType.FILE_SEND_DATA),
+                BodySize = (uint)(filePacket.Body.GetByteBufferSize()),
+                Fragmented = fragmented,
+                LastPacket = (byte)((totalRead < fileStream.Length) ? CrashPacket.ELast.NO : CrashPacket.ELast.YES),
+                Seq = packetSeq++
+            };
+
+            CrashPacket.Send(networkStream_, filePacket);
+        }
+
+        CrashPacket resultPacket = CrashPacket.Receive(networkStream_);
+        CrashPacketResultBody resultPacketBody = (CrashPacketResultBody)(resultPacket.Body);
+
+        if (resultPacketBody.Result == (byte)(CrashPacket.ESuccess.SUCCESS))
+        {
+            Console.WriteLine("successed sending crash report file...");
+        }
+        else
+        {
+            Console.WriteLine("failed sending crash report file...");
+        }
     }
 
 
