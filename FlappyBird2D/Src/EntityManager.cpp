0078bbcddfaa575c039c2557605cb30ab4e2af62
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-02-26 08:01:31 +0900

    엔티티를 관리하는 엔티티 매니저 추가

---
+++ FlappyBird2D/Src/EntityManager.cpp
@@ -0,0 +1,44 @@
+#include "Assertion.h"
+#include "EntityManager.h"
+
+EntityManager& EntityManager::Get()
+{
+	static EntityManager instance;
+	return instance;
+}
+
+void EntityManager::Startup()
+{
+	CHECK(!bIsStartup_);
+
+	cache_ = std::array<std::unique_ptr<IEntity>, MAX_RESOURCE_SIZE>();
+
+	bIsStartup_ = true;
+}
+
+void EntityManager::Shutdown()
+{
+	CHECK(bIsStartup_);
+
+	for (auto& cache : cache_)
+	{
+		if (cache && cache->IsInitialized())
+		{
+			cache->Release();
+			cache.reset();
+		}
+	}
+
+	bIsStartup_ = false;
+}
+
+void EntityManager::Destroy(const EUID& entityID)
+{
+	CHECK(0 <= entityID && entityID < cacheSize_);
+
+	if (cache_[entityID] && cache_[entityID]->IsInitialized())
+	{
+		cache_[entityID]->Release();
+		cache_[entityID].reset();
+	}
+}
