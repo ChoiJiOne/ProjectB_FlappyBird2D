e4d287e4de314b0a676679da60f72d1ce0258ba4
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-25 14:00:42 +0900

    축 정렬 경계 상자 추가
    - 충돌 처리는 구현되지 않은 상태

---
+++ FlappyBird2D/Inc/BoundBox2D.h
@@ -0,0 +1,284 @@
+#pragma once
+
+#include "Vec2.h"
+
+#include "IBound2D.h"
+
+
+/**
+ * @brief 2D 축 정렬 경계 영역입니다.
+ */
+class BoundBox2D : public IBound2D
+{
+public:
+	/**
+	 * @brief 2D 축 정렬 경계 영역의 기본 생성자입니다.
+	 */
+	explicit BoundBox2D() : IBound2D(EType::Box) {}
+
+
+	/**
+	 * @brief 2D 축 정렬 경계 영역의 생성자입니다.
+	 * 
+	 * @param minPosition 축 정렬 경계 영역의 최소값입니다.
+	 * @param maxPosition 축 정렬 경계 영역의 최댓값입니다.
+	 */
+	explicit BoundBox2D(const Vec2f& minPosition, const Vec2f& maxPosition);
+
+
+	/**
+	 * @brief 2D 축 정렬 경계 영역의 생성자입니다.
+	 * 
+	 * @param center 축 정렬 경계 영역의 중심좌표입니다.
+	 * @param width 축 정렬 경계 영역의 가로 크기입니다.
+	 * @param height 축 정렬 경계 영역의 세로 크기입니다.
+	 */
+	explicit BoundBox2D(const Vec2f& center, const float& width, const float& height);
+
+
+	/**
+	 * @brief 2D 축 정렬 경계 영역의 복사 생성자입니다.
+	 * 
+	 * @param instance 복사를 수행할 인스턴스입니다.
+	 */
+	explicit BoundBox2D(BoundBox2D&& instance) noexcept
+		: IBound2D(instance)
+		, center_(instance.center_)
+		, width_(instance.width_)
+		, height_(instance.height_)
+		, minPosition_(instance.minPosition_)
+		, maxPosition_(instance.maxPosition_) {}
+
+
+	/**
+	 * @brief 2D 축 정렬 경계 영역의 복사 생성자입니다.
+	 *
+	 * @param instance 복사를 수행할 인스턴스입니다.
+	 */
+	explicit BoundBox2D(const BoundBox2D& instance) noexcept
+		: IBound2D(instance)
+		, center_(instance.center_)
+		, width_(instance.width_)
+		, height_(instance.height_)
+		, minPosition_(instance.minPosition_)
+		, maxPosition_(instance.maxPosition_) {}
+
+
+	/**
+	 * @brief 2D 축 정렬 경계 영역의 대입 연산자입니다.
+	 * 
+	 * @param instance 대입 연산을 수행할 인스턴스입니다.
+	 * 
+	 * @return 대입을 수행한 객체의 참조자를 반환합니다.
+	 */
+	BoundBox2D& operator=(BoundBox2D&& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		IBound2D::operator=(instance);
+
+		center_ = instance.center_;
+		width_ = instance.width_;
+		height_ = instance.height_;
+		minPosition_ = instance.minPosition_;
+		maxPosition_ = instance.maxPosition_;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 2D 축 정렬 경계 영역의 대입 연산자입니다.
+	 *
+	 * @param instance 대입 연산을 수행할 인스턴스입니다.
+	 *
+	 * @return 대입을 수행한 객체의 참조자를 반환합니다.
+	 */
+	BoundBox2D& operator=(const BoundBox2D& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		IBound2D::operator=(instance);
+
+		center_ = instance.center_;
+		width_ = instance.width_;
+		height_ = instance.height_;
+		minPosition_ = instance.minPosition_;
+		maxPosition_ = instance.maxPosition_;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 2D 경계 영역 상자의 프로퍼티를 설정합니다.
+	 *
+	 * @param minPosition 축 정렬 경계 상자의 최소 좌표입니다.
+	 * @param maxPosition 축 정렬 경계 상자의 최대 좌표입니다.
+	 */
+	void SetProperty(const Vec2f& minPosition, const Vec2f& maxPosition);
+
+
+	/**
+	 * @brief 2D 경계 영역 상자의 프로퍼티를 설정합니다.
+	 *
+	 * @param center 축 정렬 경계 상자의 중심 좌표입니다.
+	 * @param width 축 정렬 경계 상자의 가로 크기입니다.
+	 * @param height 축 정렬 경계 상자의 세로 크기입니다.
+	 */
+	void SetProperty(const Vec2f& center, const float& width, const float& height);
+
+
+	/**
+	 * @brief 2D 경계 영역 상자의 중심을 설정합니다.
+	 * 
+	 * @param center 축 정렬 경계 상자의 중심입니다.
+	 */
+	void SetCenter(const Vec2f& center);
+
+
+	/**
+	 * @brief 2D 경계 영역 상자의 중심을 얻습니다.
+	 * 
+	 * @return 2D 경계 영역 상자의 중심을 반환합니다.
+	 */
+	Vec2f GetCenter() { return center_; }
+
+
+	/**
+	 * @brief 2D 경계 영역 상자의 중심을 얻습니다.
+	 *
+	 * @return 2D 경계 영역 상자의 중심을 반환합니다.
+	 */
+	const Vec2f& GetCenter() const { return center_; }
+	
+
+	/**
+	 * @brief 2D 경계 영역 상자의 가로 크기를 얻습니다.
+	 * 
+	 * @return 2D 경계 영역 상자의 가로 크기를 반환합니다.
+	 */
+	float GetWidth() { return width_; }
+
+
+	/**
+	 * @brief 2D 경계 영역 상자의 세로 크기를 얻습니다.
+	 *
+	 * @return 2D 경계 영역 상자의 세로 크기를 반환합니다.
+	 */
+	const float& GetWidth() const { return width_; }
+
+
+	/**
+	 * @brief 2D 경계 영역 상자의 세로 크기를 얻습니다.
+	 *
+	 * @return 2D 경계 영역 상자의 세로 크기를 반환합니다.
+	 */
+	float GetHeight() { return height_; }
+
+
+	/**
+	 * @brief 2D 경계 영역 상자의 세로 크기를 얻습니다.
+	 *
+	 * @return 2D 경계 영역 상자의 세로 크기를 반환합니다.
+	 */
+	const float& GetHeight() const { return height_; }
+
+
+	/**
+	 * @brief 2D 경계 영역의 최소값을 얻습니다.
+	 * 
+	 * @return 2D 경계 영역의 최소값을 반환합니다.
+	 */
+	Vec2f GetMinPosition() { return minPosition_; }
+
+
+	/**
+	 * @brief 2D 경계 영역의 최소값을 얻습니다.
+	 *
+	 * @return 2D 경계 영역의 최소값을 반환합니다.
+	 */
+	const Vec2f& GetMinPosition() const { return minPosition_; }
+
+
+	/**
+	 * @brief 2D 경계 영역의 최대값을 얻습니다.
+	 *
+	 * @return 2D 경계 영역의 최대값을 반환합니다.
+	 */
+	Vec2f GetMaxPosition() { return maxPosition_; }
+
+
+	/**
+	 * @brief 2D 경계 영역의 최대값을 얻습니다.
+	 *
+	 * @return 2D 경계 영역의 최대값을 반환합니다.
+	 */
+	const Vec2f& GetMaxPosition() const { return maxPosition_; }
+
+
+	/**
+	 * @brief 다른 2D 영역과 충돌하는지 검사합니다.
+	 *
+	 * @param bound 충돌 여부를 검사할 경계 영역입니다.
+	 *
+	 * @return 다른 경계 영역과 충돌한다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	virtual bool Intersect(const IBound2D* bound) const override;
+
+
+private:
+	/**
+	 * @brief 경계 영역의 중심 좌표와 가로/세로 크기로부터 최대/최소값을 계산합니다.
+	 * 
+	 * @param center 경계 영역의 중심좌표입니다.
+	 * @param width 경계 영역의 가로 크기입니다.
+	 * @param height 경계 영역의 세로 크기입니다.
+	 * @param outMinPosition 경계 영역의 최소값입니다.
+	 * @param outMaxPosition 경계 영역의 최대값입니다.
+	 */
+	void ComputeBoundBoxProperties(const Vec2f& center, const float& width, const float& height, Vec2f& outMinPosition, Vec2f& outMaxPosition) const;
+	
+
+	/**
+	 * @brief 경계 영역의 최대/최소값으로부터 중심 좌표와 가로/세로 크기를 계산합니다.
+	 * 
+	 * @param minPosition 경계 영역의 최소값입니다.
+	 * @param maxPosition 경계 영역의 최대값입니다.
+	 * @param outCenter 경계 영역의 중심좌표입니다.
+	 * @param outWidth 경계 영역의 가로 크기입니다.
+	 * @param outHeight 경계 영역의 세로 크기입니다.
+	 */
+	void ComputeBoundBoxProperties(const Vec2f& minPosition, const Vec2f& maxPosition, Vec2f& outCenter, float& outWidth, float& outHeight) const;
+
+
+private:
+	/**
+	 * @brief 경계 영역의 중심 좌표입니다.
+	 */
+	Vec2f center_;
+
+
+	/**
+	 * @brief 경계 영역의 가로 크기입니다.
+	 */
+	float width_ = 0.0f;
+
+
+	/**
+	 * @brief 경계 영역의 세로 크기입니다.
+	 */
+	float height_ = 0.0f;
+
+
+	/**
+	 * @brief 경계 영역의 최소값입니다.
+	 */
+	Vec2f minPosition_;
+
+
+	/**
+	 * @brief 경계 영역의 최댓값입니다.
+	 */
+	Vec2f maxPosition_;
+};
---
+++ FlappyBird2D/Src/BoundBox2D.cpp
@@ -0,0 +1,85 @@
+#include "MathModule.h"
+
+#include "Assertion.h"
+#include "BoundBox2D.h"
+
+BoundBox2D::BoundBox2D(const Vec2f& minPosition, const Vec2f& maxPosition)
+	: IBound2D(EType::Box)
+	, minPosition_(Vec2f(MathModule::Min<float>(minPosition.x, maxPosition_.x), MathModule::Min<float>(minPosition.y, maxPosition_.y)))
+	, maxPosition_(Vec2f(MathModule::Max<float>(minPosition.x, maxPosition_.x), MathModule::Max<float>(minPosition.y, maxPosition_.y)))
+{
+	ComputeBoundBoxProperties(minPosition_, maxPosition_, center_, width_, height_);
+}
+
+BoundBox2D::BoundBox2D(const Vec2f& center, const float& width, const float& height)
+	: IBound2D(EType::Box)
+	, center_(center)
+	, width_(width)
+	, height_(height)
+{
+	ComputeBoundBoxProperties(center_, width_, height_, minPosition_, maxPosition_);
+}
+
+void BoundBox2D::SetProperty(const Vec2f& minPosition, const Vec2f& maxPosition)
+{
+	minPosition_ = Vec2f(MathModule::Min<float>(minPosition.x, maxPosition_.x), MathModule::Min<float>(minPosition.y, maxPosition_.y));
+	maxPosition_ = Vec2f(MathModule::Max<float>(minPosition.x, maxPosition_.x), MathModule::Max<float>(minPosition.y, maxPosition_.y));
+
+	ComputeBoundBoxProperties(minPosition_, maxPosition_, center_, width_, height_);
+}
+
+void BoundBox2D::SetProperty(const Vec2f& center, const float& width, const float& height)
+{
+	center_ = center;
+	width_ = width;
+	height_ = height;
+
+	ComputeBoundBoxProperties(center_, width_, height_, minPosition_, maxPosition_);
+}
+
+void BoundBox2D::SetCenter(const Vec2f& center)
+{
+	center_ = center;
+
+	ComputeBoundBoxProperties(center_, width_, height_, minPosition_, maxPosition_);
+}
+
+bool BoundBox2D::Intersect(const IBound2D* bound) const
+{
+	bool bIsIntersect = false;
+	EType type = bound->GetType();
+	
+	switch (type)
+	{
+	case IBound2D::EType::Box:
+		break;
+
+	case IBound2D::EType::Circle:
+		break;
+
+	case IBound2D::EType::None:
+		bIsIntersect = false;
+		break;
+
+	default:
+		ASSERT(false, "undefined bound type");
+		break;
+	}
+
+	return bIsIntersect;
+}
+
+void BoundBox2D::ComputeBoundBoxProperties(const Vec2f& center, const float& width, const float& height, Vec2f& outMinPosition, Vec2f& outMaxPosition) const
+{
+	CHECK(width >= 0.0f && height >= 0.0f);
+
+	outMinPosition = center - Vec2f(width * 0.5f, height * 0.5f);
+	outMaxPosition = center + Vec2f(width * 0.5f, height * 0.5f);
+}
+
+void BoundBox2D::ComputeBoundBoxProperties(const Vec2f& minPosition, const Vec2f& maxPosition, Vec2f& outCenter, float& outWidth, float& outHeight) const
+{
+	outCenter = (minPosition + maxPosition) * 0.5f;
+	outWidth = (maxPosition.x - minPosition.x);
+	outHeight = (maxPosition.y - minPosition.y);
+}
