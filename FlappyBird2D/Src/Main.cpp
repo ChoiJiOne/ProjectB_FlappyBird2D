e9361bee117433b99c95451b0d669f19541a05f5
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-26 17:23:50 +0900

    입력 처리 매니저의 초기화/해제를 수행하는 기능 추가 및 입력 처리 테스트 코드 추가
    - 입력 처리 테스트 시 사용할 폰트 리소스 추가

--- FlappyBird2D/Src/Main.cpp
+++ FlappyBird2D/Src/Main.cpp
@@ -19,57 +19,64 @@
 #include "GameTimer.h"
 #include "GeometryPass2D.h"
 #include "GlyphPass2D.h"
+#include "InputManager.h"
 #include "RenderManager.h"
 #include "ResourceManager.h"
 #include "SDLManager.h"
 #include "Shader.h"
 #include "SpritePass2D.h"
+#include "StringUtils.h"
 #include "Texture2D.h"
 #include "TTFont.h"
+
+bool bIsDone = false;
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 	CHECK(CrashModule::RegisterExceptionFilter());
 
 	SDLManager::Get().Startup();
+	InputManager::Get().Startup();
 	AudioManager::Get().Startup();
-	ResourceManager::Get().Startup();
 	RenderManager::Get().Startup();
+	ResourceManager::Get().Startup();
 	EntityManager::Get().Startup();
 
 	RenderManager::Get().SetDepthMode(false);
 	RenderManager::Get().SetAlphaBlendMode(true);
 	RenderManager::Get().SetMultisampleMode(true);
 
-	RUID backgroundID = ResourceManager::Get().Create<Texture2D>("Resource/Texture/Background_Day.png");
-	RUID fontID = ResourceManager::Get().Create<TTFont>("Resource/Font/Flappy_Font.ttf", 32, 127, 64.0f);
+	InputManager::Get().AddWindowEventAction(EWindowEvent::CLOSE, [&]() {bIsDone = true; }, true);
 
+	RUID backgroundID = ResourceManager::Get().Create<Texture2D>("Resource/Texture/Background_Day.png");
+	RUID fontID = ResourceManager::Get().Create<TTFont>("Resource/Font/SeoulNamsanEB.ttf", 0x00, 0xD7A3, 12.0f);
+	
 	std::vector<EUID> entites = {
 		EntityManager::Get().Create<Background>(backgroundID)
 	};
-	
+
 	GameTimer timer;
 	timer.Reset();
 
-	bool bIsDone = false;
 	while (!bIsDone)
 	{
-		timer.Reset();
-
-		SDL_Event e;
-		while (SDL_PollEvent(&e))
-		{
-			switch (e.type)
-			{
-			case SDL_QUIT:
-				bIsDone = true;
-				break;
-			}
-		}
+		timer.Tick();
+		InputManager::Get().Tick();
 
 		EntityManager::Get().UpdateBatch(entites, timer.GetDeltaSeconds());
+		
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+
 		EntityManager::Get().RenderBatch(entites);
+
+		//if (InputManager::Get().GetMousePressState(EMouseButton::Right) == EPressState::Held)
+		//{
+		//	RenderManager::Get().RenderText2D(fontID, L"PRESS MOUSE", Vec2f(0.0f, 0.0f), Vec4f(1.0f, 0.0f, 0.0f, 1.0f), false);
+		//}
+
+		const Vec2i& mousePos = InputManager::Get().GetCurrMousePosition();
+		RenderManager::Get().RenderText2D(fontID, StringUtils::PrintF(L"(%d, %d)", mousePos.x, mousePos.y), Vec2f(0.0f, 0.0f), Vec4f(1.0f, 0.0f, 0.0f, 1.0f), false);
+
 		RenderManager::Get().EndFrame();
 	}
 
@@ -77,6 +84,7 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	ResourceManager::Get().Shutdown();
 	RenderManager::Get().Shutdown();
 	AudioManager::Get().Shutdown();
+	InputManager::Get().Shutdown();
 	SDLManager::Get().Shutdown();
 
 	CrashModule::UnregisterExceptionFilter();
