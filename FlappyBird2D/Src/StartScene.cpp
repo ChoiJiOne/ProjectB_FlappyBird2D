9c4fbb0f2a9678a975f7c71a7b749f64f5272fb4
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-29 17:18:58 +0900

    시작 씬 내의 버튼 이벤트 람다 함수를 변수로 분리

--- FlappyBird2D/Src/StartScene.cpp
+++ FlappyBird2D/Src/StartScene.cpp
@@ -23,25 +23,25 @@ void StartScene::Enter()
 {
 	CHECK(!bIsEnter_);
 
+	static auto startEvent = [&]()
+	{
+		link_ = playScene_;
+		bDetectSwitch_ = true;
+	};
+
+	static auto settingEvent = [&]()
+	{
+		link_ = settingScene_;
+		bDetectSwitch_ = true;
+	};
+
 	static RUID font90ID = ResourceManager::Get().Create<TTFont>("Resource/Font/Flappy_Font.ttf", 0x00, 127, 90.0f);
 	static RUID font32ID = ResourceManager::Get().Create<TTFont>("Resource/Font/Flappy_Font.ttf", 0x00, 127, 32.0f);
 
 	static RUID background = EntityManager::Get().Create<Background>();
 	static RUID mainTitle = EntityManager::Get().Create<MainTitle>(font90ID);
-	static RUID startButton = EntityManager::Get().Create<Button>("Resource/Button/Start.json", font32ID, EMouseButton::Left, 
-		[&]() 
-		{ 
-			link_ = playScene_;
-			bDetectSwitch_ = true;
-		}
-	);
-	static RUID settingButton = EntityManager::Get().Create<Button>("Resource/Button/Setting.json", font32ID, EMouseButton::Left, 
-		[&]() 
-		{ 
-			link_ = settingScene_;
-			bDetectSwitch_ = true;
-		}
-	);
+	static RUID startButton = EntityManager::Get().Create<Button>("Resource/Button/Start.json", font32ID, EMouseButton::Left, startEvent);
+	static RUID settingButton = EntityManager::Get().Create<Button>("Resource/Button/Setting.json", font32ID, EMouseButton::Left, settingEvent);
 	static RUID quitButton = EntityManager::Get().Create<Button>("Resource/Button/Quit.json", font32ID, EMouseButton::Left, quitLoopEvent_);
 
 	entities_ = { background, mainTitle, startButton, settingButton, quitButton, };
