cf5ff4ff043a6552c47cccc245cd61254bffb302
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-29 17:56:47 +0900

    게임 플레이 씬 추가
    - 현재 백그라운드 배경만 추가된 상태

---
+++ FlappyBird2D/Inc/PlayScene.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+
+#include "IEntity.h"
+#include "IScene.h"
+
+
+/**
+ * @brief 플레이 씬입니다.
+ */
+class PlayScene : public IScene
+{
+public:
+	/**
+	 * @brief 플레이 씬의 디폴트 생성자입니다.
+	 */
+	PlayScene() = default;
+
+
+	/**
+	 * @brief 플레이 씬의 가상 소멸자입니다.
+	 */
+	virtual ~PlayScene() {}
+
+
+	/**
+	 * @brief 플레이 씬의 복사 생성자 및 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(PlayScene);
+
+
+	/**
+	 * @brief 플레이 씬의 한 프레임을 수행합니다.
+	 *
+	 * @param deltaSeconds 초 단위 델타 시간값입니다.
+	 */
+	virtual void Tick(float deltaSeconds) override;
+
+
+	/**
+	 * @brief 플레이 씬에 진입합니다.
+	 */
+	virtual void Enter() override;
+
+
+	/**
+	 * @brief 플레이 씬에서 나갑니다.
+	 */
+	virtual void Exit() override;
+
+
+	/**
+	 * @brief 시작 씬을 연결합니다.
+	 *
+	 * @param startScene 설정할 시작 씬의 포인터입니다.
+	 */
+	void SetStartScene(IScene* startScene) { startScene_ = startScene; }
+
+
+private:
+	/**
+	 * @brief 플레이 씬 입니다.
+	 */
+	IScene* startScene_ = nullptr;
+
+
+	/**
+	 * @brief 엔티티 목록입니다.
+	 */
+	std::vector<EUID> entities_;
+};
---
+++ FlappyBird2D/Src/PlayScene.cpp
@@ -0,0 +1,40 @@
+#include "Assertion.h"
+#include "Background.h"
+#include "Button.h"
+#include "ConfigManager.h"
+#include "EntityManager.h"
+#include "Panel.h"
+#include "RenderManager.h"
+#include "ResourceManager.h"
+#include "PlayScene.h"
+#include "TTFont.h"
+
+void PlayScene::Tick(float deltaSeconds)
+{
+	EntityManager::Get().UpdateBatch(entities_, deltaSeconds);
+
+	RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+
+	EntityManager::Get().RenderBatch(entities_);
+
+	RenderManager::Get().EndFrame();
+}
+
+void PlayScene::Enter()
+{
+	CHECK(!bIsEnter_);
+
+	static RUID background = EntityManager::Get().Create<Background>();
+
+	entities_ = { background, };
+
+	bIsEnter_ = true;
+}
+
+void PlayScene::Exit()
+{
+	CHECK(bIsEnter_);
+
+	bDetectSwitch_ = false;
+	bIsEnter_ = false;
+}
