267d3679065f024f2cdd795510fc6895af17ea04
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-29 17:43:45 +0900

    메인 함수에 시작 씬과 세팅 씬 추가

--- FlappyBird2D/Src/Main.cpp
+++ FlappyBird2D/Src/Main.cpp
@@ -27,8 +27,10 @@
 #include "RenderManager.h"
 #include "ResourceManager.h"
 #include "SDLManager.h"
+#include "SettingScene.h"
 #include "Shader.h"
 #include "SpritePass2D.h"
+#include "StartScene.h"
 #include "StringUtils.h"
 #include "Texture2D.h"
 #include "TTFont.h"
@@ -51,61 +53,21 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	RenderManager::Get().SetAlphaBlendMode(true);
 	RenderManager::Get().SetMultisampleMode(true);
 
-	InputManager::Get().AddWindowEventAction(EWindowEvent::CLOSE, [&]() {bIsDone = true; }, true);
+	std::function<void()> quitLoopEvent = [&]() { bIsDone = true; };
 
-	RUID font90ID = ResourceManager::Get().Create<TTFont>("Resource/Font/Flappy_Font.ttf", 0x00, 127, 90.0f);
-	RUID font64ID = ResourceManager::Get().Create<TTFont>("Resource/Font/Flappy_Font.ttf", 0x00, 127, 64.0f);
-	RUID font32ID = ResourceManager::Get().Create<TTFont>("Resource/Font/Flappy_Font.ttf", 0x00, 127, 32.0f);
+	InputManager::Get().AddWindowEventAction(EWindowEvent::CLOSE, quitLoopEvent, true);
+
+	StartScene startScene;
+	SettingScene settingScene;
+
+	startScene.SetQuitLoopEvent(quitLoopEvent);
+	startScene.SetSettingScene(&settingScene);
+
+	settingScene.SetStartScene(&startScene);
+
+	IScene* currentScene = &startScene;
+	currentScene->Enter();
 	
-	//std::vector<EUID> entites = 
-	//{
-	//	EntityManager::Get().Create<Background>(),
-	//	EntityManager::Get().Create<MainTitle>(font90ID),
-	//	EntityManager::Get().Create<Button>("Resource/Button/Start.json", font32ID, EMouseButton::Left, [&]() { SDL_Log("CLICK START!"); }),
-	//	EntityManager::Get().Create<Button>("Resource/Button/Setting.json", font32ID, EMouseButton::Left, [&]() { SDL_Log("CLICK SETTING!"); }),
-	//	EntityManager::Get().Create<Button>("Resource/Button/Quit.json", font32ID, EMouseButton::Left, [&]() { bIsDone = true; }),
-	//};
-
-	std::vector<EUID> entites = 
-	{
-		EntityManager::Get().Create<Background>(),
-		EntityManager::Get().Create<Panel>("Resource/Panel/Setting.json", font64ID),
-		EntityManager::Get().Create<Panel>("Resource/Panel/Background.json", font32ID),
-		EntityManager::Get().Create<Panel>("Resource/Panel/Level.json", font32ID),
-		EntityManager::Get().Create<Panel>("Resource/Panel/Bird.json", font32ID),
-
-		EntityManager::Get().Create<Button>("Resource/Button/Day.json", font32ID, EMouseButton::Left, [&]() {
-			ConfigManager::Get().SetCurrentBackgroundID(ConfigManager::EBackground::Day);
-		}),
-		EntityManager::Get().Create<Button>("Resource/Button/Night.json", font32ID, EMouseButton::Left, [&]() {
-			ConfigManager::Get().SetCurrentBackgroundID(ConfigManager::EBackground::Night);
-		}),
-
-		EntityManager::Get().Create<Button>("Resource/Button/Easy.json", font32ID, EMouseButton::Left, [&]() {
-			ConfigManager::Get().SetCurrentLevel(ConfigManager::ELevel::Easy);
-		}),
-		EntityManager::Get().Create<Button>("Resource/Button/Normal.json", font32ID, EMouseButton::Left, [&]() {
-			ConfigManager::Get().SetCurrentLevel(ConfigManager::ELevel::Normal);
-		}),
-		EntityManager::Get().Create<Button>("Resource/Button/Hard.json", font32ID, EMouseButton::Left, [&]() {
-			ConfigManager::Get().SetCurrentLevel(ConfigManager::ELevel::Hard);
-		}),
-
-		EntityManager::Get().Create<Button>("Resource/Button/Blue.json", font32ID, EMouseButton::Left, [&]() {
-			ConfigManager::Get().SetCurrentBird(ConfigManager::EBird::Blue);
-		}),
-		EntityManager::Get().Create<Button>("Resource/Button/Red.json", font32ID, EMouseButton::Left, [&]() {
-			ConfigManager::Get().SetCurrentBird(ConfigManager::EBird::Red);
-		}),
-		EntityManager::Get().Create<Button>("Resource/Button/Yellow.json", font32ID, EMouseButton::Left, [&]() {
-			ConfigManager::Get().SetCurrentBird(ConfigManager::EBird::Yellow);
-		}),
-		
-		EntityManager::Get().Create<Button>("Resource/Button/Back.json", font32ID, EMouseButton::Left, [&]() {
-			SDL_Log("CLICK BACK!");
-		}),
-	};
-
 	GameTimer timer;
 	timer.Reset();
 
@@ -114,15 +76,16 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 		timer.Tick();
 		InputManager::Get().Tick();
 
-		EntityManager::Get().UpdateBatch(entites, timer.GetDeltaSeconds());
+		currentScene->Tick(timer.GetDeltaSeconds());
 		
-		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
-
-		EntityManager::Get().RenderBatch(entites);
-
-		RenderManager::Get().EndFrame();
+		if (currentScene->IsDetectSwitch())
+		{
+			currentScene->Exit();
+			currentScene = currentScene->GetLink();
+			currentScene->Enter();
+		}
 	}
-
+	
 	ConfigManager::Get().Shutdown();
 	EntityManager::Get().Shutdown();
 	ResourceManager::Get().Shutdown();
