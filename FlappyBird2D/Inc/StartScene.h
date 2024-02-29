8b647cf008968a726eaf3f0a26d281cd1123fc29
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-29 17:14:29 +0900

    시작 씬 추가
    - 현재 버튼의 이벤트는 추가되지 않은 상태

---
+++ FlappyBird2D/Inc/StartScene.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+
+#include "IEntity.h"
+#include "IScene.h"
+
+
+/**
+ * @brief 시작 씬입니다.
+ */
+class StartScene : public IScene
+{
+public:
+	/**
+	 * @brief 시작 씬의 디폴트 생성자입니다.
+	 */
+	StartScene() = default;
+
+
+	/**
+	 * @brief 시작 씬의 가상 소멸자입니다.
+	 */
+	virtual ~StartScene() {}
+
+
+	/**
+	 * @brief 시작 씬의 복사 생성자 및 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(StartScene);
+
+
+	/**
+	 * @brief 시작 씬의 한 프레임을 수행합니다.
+	 *
+	 * @param deltaSeconds 초 단위 델타 시간값입니다.
+	 */
+	virtual void Tick(float deltaSeconds) override;
+
+
+	/**
+	 * @brief 시작 씬에 진입합니다.
+	 */
+	virtual void Enter() override;
+
+
+	/**
+	 * @brief 시작 씬에서 나갑니다.
+	 */
+	virtual void Exit() override;
+
+
+	/**
+	 * @brief 루프 종료 이벤트를 설정합니다.
+	 * 
+	 * @param quitLoopEvent 설정할 루프 종료 이벤트입니다.
+	 */
+	void SetQuitLoopEvent(const std::function<void()>& quitLoopEvent) { quitLoopEvent_ = quitLoopEvent;	}
+
+
+	/**
+	 * @brief 플레이 씬을 연결합니다.
+	 * 
+	 * @param playScene 설정할 플레이 씬의 포인터입니다.
+	 */
+	void SetPlayScene(IScene* playScene) { playScene_ = playScene; }
+
+
+	/**
+	 * @brief 설정 씬을 연결합니다.
+	 * 
+	 * @param settingScene 설정할 설정 씬의 포인터입니다.
+	 */
+	void SetSettingScene(IScene* settingScene) { settingScene_ = settingScene; }
+
+
+private:
+	/**
+	 * @brief 루프 종료 이벤트입니다.
+	 */
+	std::function<void()> quitLoopEvent_;
+
+
+	/**
+	 * @brief 플레이 씬 입니다.
+	 */
+	IScene* playScene_ = nullptr;
+
+
+	/**
+	 * @brief 세팅 씬입니다.
+	 */
+	IScene* settingScene_ = nullptr;
+
+
+	/**
+	 * @brief 엔티티 목록입니다.
+	 */
+	std::vector<EUID> entities_;
+};
---
+++ FlappyBird2D/Src/StartScene.cpp
@@ -0,0 +1,58 @@
+#include "Assertion.h"
+#include "Background.h"
+#include "Button.h"
+#include "EntityManager.h"
+#include "MainTitle.h"
+#include "RenderManager.h"
+#include "ResourceManager.h"
+#include "StartScene.h"
+#include "TTFont.h"
+
+void StartScene::Tick(float deltaSeconds)
+{
+	EntityManager::Get().UpdateBatch(entities_, deltaSeconds);
+
+	RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+
+	EntityManager::Get().RenderBatch(entities_);
+
+	RenderManager::Get().EndFrame();
+}
+
+void StartScene::Enter()
+{
+	CHECK(!bIsEnter_);
+
+	static RUID font90ID = ResourceManager::Get().Create<TTFont>("Resource/Font/Flappy_Font.ttf", 0x00, 127, 90.0f);
+	static RUID font32ID = ResourceManager::Get().Create<TTFont>("Resource/Font/Flappy_Font.ttf", 0x00, 127, 32.0f);
+
+	static RUID background = EntityManager::Get().Create<Background>();
+	static RUID mainTitle = EntityManager::Get().Create<MainTitle>(font90ID);
+	static RUID startButton = EntityManager::Get().Create<Button>("Resource/Button/Start.json", font32ID, EMouseButton::Left, 
+		[&]() 
+		{ 
+			link_ = playScene_;
+			bDetectSwitch_ = true;
+		}
+	);
+	static RUID settingButton = EntityManager::Get().Create<Button>("Resource/Button/Setting.json", font32ID, EMouseButton::Left, 
+		[&]() 
+		{ 
+			link_ = settingScene_;
+			bDetectSwitch_ = true;
+		}
+	);
+	static RUID quitButton = EntityManager::Get().Create<Button>("Resource/Button/Quit.json", font32ID, EMouseButton::Left, quitLoopEvent_);
+
+	entities_ = { background, mainTitle, startButton, settingButton, quitButton, };
+
+	bIsEnter_ = true;
+}
+
+void StartScene::Exit()
+{
+	CHECK(bIsEnter_);
+
+	bDetectSwitch_ = false;
+	bIsEnter_ = false;
+}
