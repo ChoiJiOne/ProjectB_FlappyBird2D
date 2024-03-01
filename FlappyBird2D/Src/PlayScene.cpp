b3fc7ef04f4b01a22a6a21ebb6677e1fbab2c515
CHOIJIONE\choij <choijione02@gmail.com>
2024-03-01 16:36:01 +0900

    플레이 씬에 파이프 엔티티 추가

--- FlappyBird2D/Src/PlayScene.cpp
+++ FlappyBird2D/Src/PlayScene.cpp
@@ -5,6 +5,7 @@
 #include "EntityManager.h"
 #include "Land.h"
 #include "Panel.h"
+#include "Pipe.h"
 #include "RenderManager.h"
 #include "ResourceManager.h"
 #include "PlayScene.h"
@@ -28,7 +29,22 @@ void PlayScene::Enter()
 	static EUID background = EntityManager::Get().Create<Background>(50.0f);
 	static EUID land = EntityManager::Get().Create<Land>(200.0f);
 
-	entities_ = { background, land };
+	int32_t w = 0;
+	int32_t h = 0;
+	float gap = 50.0f;
+	RenderManager::Get().GetRenderTargetWindowSize(w, h);
+
+	Pipe::SetStartLocation(Vec2f(static_cast<float>(w) + gap, static_cast<float>(h) * 0.4f));
+	Pipe::SetEndLocation(Vec2f(-gap, static_cast<float>(h) * 0.4f));
+
+	static EUID pipe = EntityManager::Get().Create<Pipe>(200.0f);
+	
+	entities_ = 
+	{ 
+		background, 
+		pipe,
+		land, 
+	};
 
 	bIsEnter_ = true;
 }
