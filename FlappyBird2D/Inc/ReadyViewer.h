502bca7c7ec7aac319868035ff3b6a061b75d95a
CHOIJIONE\choij <choijione02@gmail.com>
2024-03-03 11:14:36 +0900

    준비(Ready) 상태를 보여주는 엔티티 추가

---
+++ FlappyBird2D/Inc/ReadyViewer.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "Vec2.h"
+
+#include "IEntity.h"
+#include "IResource.h"
+
+
+/**
+ * @brief 준비(Ready) 상태를 표시하는 엔티티입니다.
+ */
+class ReadyViewer : public IEntity
+{
+public:
+	/**
+	 * @brief 준비(Ready) 상태를 표시하는 엔티티의 생성자입니다.
+	 */
+	explicit ReadyViewer();
+
+
+	/**
+	 * @brief 준비(Ready) 상태를 표시하는 엔티티의 가상 소멸자입니다.
+	 */
+	virtual ~ReadyViewer();
+
+
+	/**
+	 * @brief 준비(Ready) 상태를 표시하는 엔티티의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(ReadyViewer);
+
+
+	/**
+	 * @brief 준비(Ready) 상태를 표시하는 엔티티를 업데이트합니다.
+	 *
+	 * @param deltaSeconds 델타 시간 값입니다.
+	 */
+	virtual void Tick(float deltaSeconds) override;
+
+
+	/**
+	 * @brief 준비(Ready) 상태를 표시하는 엔티티를 화면에 그립니다.
+	 */
+	virtual void Render() override;
+
+
+	/**
+	 * @brief 준비(Ready) 상태를 표시하는 엔티티 내의 리소스를 할당 해제합니다.
+	 */
+	virtual void Release() override;
+
+
+private:
+	/**
+	 * @brief 게임 플레이 정보를 표시할 텍스처입니다.
+	 */
+	RUID infoID_ = -1;
+
+
+	/**
+	 * @brief 게임 플레이 정보를 표시할 텍스처의 위치입니다.
+	 */
+	Vec2f infoLocation_;
+
+
+	/**
+	 * @brief 게임 플레이 정보를 표시할 텍스처의 가로 크기입니다.
+	 */
+	float infoWidth_ = 0.0f;
+
+
+	/**
+	 * @brief 게임 플레이 정보를 표시할 텍스처의 세로 크기입니다.
+	 */
+	float infoHeight_ = 0.0f;
+
+
+	/**
+	 * @brief Get Ready 텍스처입니다.
+	 */
+	RUID getReadyID_ = -1;
+
+
+	/**
+	 * @brief Get Ready 텍스처의 위치입니다.
+	 */
+	Vec2f getReadyLocation_;
+
+
+	/**
+	 * @brief Get Ready 텍스처의 가로 크기입니다.
+	 */
+	float getReadyWidth_ = 0.0f;
+
+
+	/**
+	 * @brief Get Ready 텍스처의 세로 크기입니다.
+	 */
+	float getReadyHeight_ = 0.0f;
+
+
+	/**
+	 * @brief 누적 시간값입니다.
+	 */
+	float accumulateTime_ = 0.0f;
+};
---
+++ FlappyBird2D/Src/ReadyViewer.cpp
@@ -0,0 +1,51 @@
+#include "MathModule.h"
+
+#include "Assertion.h"
+#include "ReadyViewer.h"
+#include "RenderManager.h"
+#include "ResourceManager.h"
+#include "Texture2D.h"
+
+ReadyViewer::ReadyViewer()
+{
+	static RUID infoID = ResourceManager::Get().Create<Texture2D>("Resource/Texture/GameInfo.png");
+	infoID_ = infoID;
+	infoLocation_ = Vec2f(300.0f, 400.0f);
+	infoWidth_ = 150.0f;
+	infoHeight_ = 140.0f;
+
+	static RUID getReadyID = ResourceManager::Get().Create<Texture2D>("Resource/Texture/GetReady.png");
+	getReadyID_ = getReadyID;
+	getReadyLocation_ = Vec2f(300.0f, 200.0f);
+	getReadyWidth_ = 350.0f;
+	getReadyHeight_ = 100.0f;
+
+	bIsInitialized_ = true;
+}
+
+ReadyViewer::~ReadyViewer()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void ReadyViewer::Tick(float deltaSeconds)
+{
+	accumulateTime_ += deltaSeconds;
+}
+
+void ReadyViewer::Render()
+{
+	RenderManager::Get().RenderSprite2D(getReadyID_, getReadyLocation_ + Vec2f(0.0f, 20.0f * MathModule::Sin(accumulateTime_ * 2.0f)), getReadyWidth_, getReadyHeight_, 0.0f);
+	RenderManager::Get().RenderSprite2D(infoID_, infoLocation_, infoWidth_, infoHeight_, 0.0f);
+}
+
+void ReadyViewer::Release()
+{
+	if (bIsInitialized_)
+	{
+		bIsInitialized_ = false;
+	}
+}
