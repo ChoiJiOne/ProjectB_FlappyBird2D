7bd94df3603961756d51ce63a44ba2e461af5b9a
CHOIJIONE\choij <choijione02@gmail.com>
2024-03-06 16:09:21 +0900

    플레이어의 최고 기록을 Cache 폴더 하위의 Best.bin 파일에 기록하는 기능 추가

--- FlappyBird2D/Src/ConfigManager.cpp
+++ FlappyBird2D/Src/ConfigManager.cpp
@@ -1,7 +1,22 @@
+#include "FileModule.h"
+
 #include "Assertion.h"
 #include "ConfigManager.h"
 #include "ResourceManager.h"
 #include "Texture2D.h"
+
+/**
+ * @brief 레벨에 따른 최고 스코어를 기록하는 파일의 포멧입니다.
+ */
+struct BestScoreFileFormat
+{
+	ConfigManager::ELevel easy;
+	int32_t easyScore;
+	ConfigManager::ELevel normal;
+	int32_t normalScore;
+	ConfigManager::ELevel hard;
+	int32_t hardScore;
+};
 
 ConfigManager& ConfigManager::Get()
 {
@@ -27,12 +42,43 @@ void ConfigManager::Startup()
 		{ ELevel::Hard,   -1 },
 	};
 
-	bestScore_ =
+	if (FileModule::IsValidPath("Resource/Cache"))
 	{
-		{ ELevel::Easy,   -1 },
-		{ ELevel::Normal, -1 },
-		{ ELevel::Hard,   -1 },
-	};
+		if (FileModule::IsValidPath("Resource/Cache/Best.bin"))
+		{
+			std::vector<uint8_t> buffer;
+			CHECK(FileModule::ReadBufferFromFile("Resource/Cache/Best.bin", buffer));
+
+			BestScoreFileFormat* bestScoreFilePtr = reinterpret_cast<BestScoreFileFormat*>(buffer.data());
+
+			bestScore_ =
+			{
+				{ bestScoreFilePtr->easy,   bestScoreFilePtr->easyScore   },
+				{ bestScoreFilePtr->normal, bestScoreFilePtr->normalScore },
+				{ bestScoreFilePtr->hard,   bestScoreFilePtr->hardScore   },
+			};
+		}
+		else
+		{
+			bestScore_ =
+			{
+				{ ELevel::Easy,   -1 },
+				{ ELevel::Normal, -1 },
+				{ ELevel::Hard,   -1 },
+			};
+		}
+	}
+	else
+	{
+		CHECK(FileModule::MakeDirectory("Resource/Cache"));
+
+		bestScore_ =
+		{
+			{ ELevel::Easy,   -1 },
+			{ ELevel::Normal, -1 },
+			{ ELevel::Hard,   -1 },
+		};
+	}
 
 	bIsStartup_ = true;
 }
@@ -40,6 +86,27 @@ void ConfigManager::Startup()
 void ConfigManager::Shutdown()
 {
 	CHECK(bIsStartup_);
+
+	if (!FileModule::IsValidPath("Resource/Cache"))
+	{
+		CHECK(FileModule::MakeDirectory("Resource/Cache"));
+	}
+
+	BestScoreFileFormat bestScoreFile;
+	bestScoreFile.easy = ELevel::Easy;
+	bestScoreFile.easyScore = bestScore_[ELevel::Easy];
+	bestScoreFile.normal = ELevel::Normal;
+	bestScoreFile.normalScore = bestScore_[ELevel::Normal];
+	bestScoreFile.hard = ELevel::Hard;
+	bestScoreFile.hardScore = bestScore_[ELevel::Hard];
+
+	BestScoreFileFormat* bestScoreFilePtr = &bestScoreFile;
+	std::size_t bufferSize = sizeof(bestScoreFile);
+
+	std::vector<uint8_t> buffer(bufferSize);
+	std::memcpy(buffer.data(), bestScoreFilePtr, bufferSize);
+
+	CHECK(FileModule::WriteBufferToFile("Resource/Cache/Best.bin", buffer));
 
 	bIsStartup_ = false;
 }
